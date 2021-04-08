1b4926369670904657dc366cede96e4e8b9f0047
mistakia <1823355+mistakia@users.noreply.github.com>
2021-04-07 20:41:39 -0400

    initial commit

---
+++ db/schema.sql
@@ -0,0 +1,62 @@
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `accounts`
+--
+
+DROP TABLE IF EXISTS `accounts`;
+
+CREATE TABLE `accounts` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `pub` varchar(255) NOT NULL,
+  `prv` varchar(255) NOT NULL,
+  `address` char(65) NOT NULL,
+  `signature` varchar(255) NOT NULL,
+  `alias` varchar(255) DEFAULT NULL,
+  `verified` tinyint(1) DEFAULT 0,
+  `last_visit` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+DROP TABLE IF EXISTS `posts`;
+
+CREATE TABLE `posts` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `url` varchar(255) NOT NULL,
+  `author` varchar(32) NOT NULL,
+  `date` int(11) NOT NULL,
+  `summary` int(11) DEFAULT NULL,
+  `score` int(11) NOT NULL DEFAULT 1,
+  `social_score` int(11) NOT NULL DEFAULT 0,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `published_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `url` (`url`),
+  KEY `title` (`title`),
+  KEY `author` (`author`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `post_tags`
+--
+
+DROP TABLE IF EXISTS `post_tags`;
+
+CREATE TABLE `post_tags` (
+  `post_id` int(11) NOT NULL,
+  `tag` varchar(65) NOT NULL,
+  `account_id` int(11) NOT NULL,
+  KEY `post_id` (`post_id`),
+  KEY `tag` (`tag`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
