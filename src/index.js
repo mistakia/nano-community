7ec30f28b3fbf0383ce450c80bb6ac3a1e6978c8
mistakia <1823355+mistakia@users.noreply.github.com>
2021-05-14 03:31:33 -0400

    fix: static react loading + robots.txt

--- src/index.js
+++ src/index.js
@@ -2,15 +2,11 @@
 import '@babel/polyfill'
 
 import React from 'react'
-import { hydrate, render } from 'react-dom'
+import { render } from 'react-dom'
 
 import Root from '@views/root'
 
 document.addEventListener('DOMContentLoaded', () => {
   const rootElement = document.getElementById('app')
-  if (rootElement.hasChildNodes()) {
-    hydrate(<Root />, rootElement)
-  } else {
-    render(<Root />, rootElement)
-  }
+  render(<Root />, rootElement)
 })
