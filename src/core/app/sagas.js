ce611729c94948fe0827bc07e911e195de241e7b
mistakia <1823355+mistakia@users.noreply.github.com>
2021-05-16 02:44:48 -0400

    feat: add cookie-less GA

--- src/core/app/sagas.js
+++ src/core/app/sagas.js
@@ -1,8 +1,29 @@
 import { takeLatest, fork, select } from 'redux-saga/effects'
 import { LOCATION_CHANGE } from 'connected-react-router'
+import FingerprintJS from '@fingerprintjs/fingerprintjs'
 
+import history from '@core/history'
 import { getApp } from './selectors'
 import { appActions } from './actions'
+
+const fpPromise = FingerprintJS.load()
+
+// cookie-less / anonymous GA reporting
+async function pageView() {
+  if (!window.gtag) {
+    return
+  }
+
+  const fp = await fpPromise
+  const result = await fp.get()
+
+  gtag('config', 'G-CV3N29BMWR', {
+    page_path: history.location.pathname,
+    client_storage: 'none',
+    anonymize_ip: true,
+    client_id: result.visitorId
+  })
+}
 
 export function* init() {
   const { token, key } = yield select(getApp)
@@ -13,6 +34,7 @@ export function* init() {
 
 export function reset() {
   window.scrollTo(0, 0)
+  pageView()
 }
 
 //= ====================================
