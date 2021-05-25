110578c296961f19cb50eeb242a4116f0aa08743
mistakia <1823355+mistakia@users.noreply.github.com>
2021-05-24 23:46:41 -0400

    fix: remove duplicates from trending

--- src/core/postlists/selectors.js
+++ src/core/postlists/selectors.js
@@ -11,5 +11,13 @@ export function getPostsForPostlistId(state, { id }) {
   const postlist = postlists.get(id, new Postlist())
   const posts = getPosts(state)
 
+  if (id === 'trending') {
+    const top = postlists.get('top', new Postlist())
+    const announcements = postlists.get('announcements', new Postlist())
+    const ids = top.postIds.merge(announcements.postIds)
+    const filtered = postlist.postIds.filter((id) => !ids.includes(id))
+    return filtered.map((id) => posts.get(id))
+  }
+
   return postlist.postIds.map((id) => posts.get(id))
 }
