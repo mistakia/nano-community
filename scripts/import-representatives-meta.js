c91dfbda2d392254ed9b20a69f052752dbc9602e
mistakia <1823355+mistakia@users.noreply.github.com>
2021-05-30 15:41:14 -0400

    feat: import representatives meta

---
+++ scripts/import-representatives-meta.js
@@ -0,0 +1,78 @@
+const debug = require('debug')
+const moment = require('moment')
+const fetchCheerioObject = require('fetch-cheerio-object')
+
+const { request } = require('../common')
+const db = require('../db')
+
+const logger = debug('import:representatives:meta')
+debug.enable('import:representatives:meta')
+
+const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+const timestamp = Math.round(Date.now() / 1000)
+
+const main = async () => {
+  const reps = await db('accounts').where({ representative: true })
+
+  for (const rep of reps) {
+    const url = `https://mynano.ninja/api/accounts/${rep.account}`
+    let res
+
+    try {
+      res = await request({ url })
+    } catch (err) {
+      console.log(err)
+    }
+
+    if (!res) {
+      continue
+    }
+
+    const html_url = `https://mynano.ninja/account/${res.slug || res.account}`
+    const sanitize = (str) => str.replace(/(reddit|github|discord|twitter)/ig, '').replace(/\r?\n|\r/g, '').trim()
+    const $ = await fetchCheerioObject(html_url)
+    const reddit = sanitize($('.fa-reddit').parent().text())
+    const twitter = sanitize($('.fa-twitter').parent().text())
+    const github = sanitize($('.fa-github').parent().text())
+    const discord = sanitize($('.fa-discord').parent().text())
+
+    const meta = {
+      account: res.account,
+
+      cpu_description: res.server ? res.server.cpu : null,
+      ram_description: res.server ? res.server.ram : null,
+      type: res.server ? res.server.type : null,
+      provider: res.network ? res.network.provider : null,
+      mynano_ninja: html_url,
+      created_at: moment(res.created).unix(),
+      description: res.description,
+
+      reddit,
+      github,
+      twitter,
+      discord,
+
+      timestamp
+    }
+
+    logger(`saving meta for account: ${meta.account}`)
+    await db('representatives_meta').insert(meta)
+    await wait(1500)
+  }
+}
+
+module.exprots = main
+
+if (!module.parent) {
+  const init = async () => {
+    await main()
+    process.exit()
+  }
+
+  try {
+    init()
+  } catch (err) {
+    console.log(err)
+    process.exit()
+  }
+}
