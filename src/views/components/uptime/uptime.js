7dfd36dc5c180552656c8d8f22de9cb1c8c1f3a8
mistakia <1823355+mistakia@users.noreply.github.com>
2021-07-02 18:25:12 -0400

    feat: use svg for uptime display

--- src/views/components/uptime/uptime.js
+++ src/views/components/uptime/uptime.js
@@ -3,6 +3,9 @@ import PropTypes from 'prop-types'
 
 import './uptime.styl'
 
+const online = '#3bd671'
+const offline = '#ee6666'
+
 export default class Uptime extends React.Component {
   render() {
     const { data, length } = this.props
@@ -11,11 +14,24 @@ export default class Uptime extends React.Component {
     const sliced = length ? data.slice(0, length) : data
     sliced.forEach((d, key) =>
       ticks.push(
-        <div key={key} className='uptime-tick' data-online={d.online} />
+        <rect
+          key={key}
+          height='14'
+          width='3'
+          y='0'
+          x={key * 5}
+          rx='1.625'
+          ry='1.625'
+          fill={d.online ? online : offline}
+        />
       )
     )
 
-    return <div className='uptime'>{ticks}</div>
+    return (
+      <svg height='14' className='uptime'>
+        {ticks}
+      </svg>
+    )
   }
 }
 
