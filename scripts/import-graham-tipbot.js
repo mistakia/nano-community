db9770c0b981e9fd181b8b7e2ab2c88accf57a11
mistakia <1823355+mistakia@users.noreply.github.com>
2021-07-18 18:11:03 -0400

    feat: import graham tipbot addresses

---
+++ scripts/import-graham-tipbot.js
@@ -0,0 +1,47 @@
+const debug = require('debug')
+
+const { request } = require('../common')
+const db = require('../db')
+
+const logger = debug('script')
+debug.enable('script')
+
+const main = async () => {
+  const url = 'https://nanobotapi.banano.cc/users'
+  const res = await request({ url })
+
+  const inserts = res.map((i) => ({
+    account: i.address,
+    alias: `Graham TipBot Discord ${i.user_last_known_name}`
+  }))
+
+  if (inserts.length) {
+    logger(`saving aliases for ${inserts.length} accounts`)
+    for (let i = 0; i < inserts.length; i += 1000) {
+      await db('accounts')
+        .insert(inserts.slice(i, i + 1000))
+        .onConflict()
+        .merge()
+    }
+  }
+}
+
+module.exprots = main
+
+if (!module.parent) {
+  const init = async () => {
+    try {
+      await main()
+    } catch (err) {
+      console.log(err)
+    }
+    process.exit()
+  }
+
+  try {
+    init()
+  } catch (err) {
+    console.log(err)
+    process.exit()
+  }
+}
