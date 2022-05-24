07a245d4ccdf4c6117cda6b629be66720dc3bd4d
mistakia <1823355+mistakia@users.noreply.github.com>
2022-05-24 12:19:36 -0400

    feat: analyze historical online weight

---
+++ scripts/analyze-online-weight.js
@@ -0,0 +1,84 @@
+const debug = require('debug')
+const percentile = require('percentile')
+const { default: PQueue } = require('p-queue')
+
+const db = require('../db')
+
+const queue = new PQueue({ concurrency: 10 })
+const log = debug('analyze-online-weight')
+debug.enable('analyze-online-weight')
+
+const percentiles = [0, 25, 50, 75, 100]
+const results = {}
+const value_types = ['online_weight', 'trended_weight']
+value_types.forEach((i) => {
+  results[i] = {}
+  percentiles.forEach((p) => (results[i][`p${p}`] = Infinity))
+})
+
+const main = async () => {
+  const timestamps = await db('voting_weight')
+    .select('timestamp')
+    .groupBy('timestamp')
+
+  const update = (values, value_type) => {
+    const r = results[value_type]
+    values.forEach((v, idx) => {
+      const key = `p${percentiles[idx]}`
+      if (v < r[key]) {
+        r[key] = v
+      }
+    })
+  }
+
+  let count = 0
+  for (const timestamp of timestamps) {
+    queue.add(async () => {
+      if (process.stdout.clearLine) {
+        process.stdout.clearLine()
+        process.stdout.cursorTo(0)
+        process.stdout.write(`Processing: ${count++}/${timestamps.length}`)
+      }
+
+      const measurements = await db('voting_weight').where(
+        'timestamp',
+        timestamp
+      )
+      const online_weight = percentile(
+        percentiles,
+        measurements.map((p) => p.online_stake_total)
+      )
+      const trended_weight = percentile(
+        percentiles,
+        measurements.map((p) => p.trended_stake_total)
+      )
+
+      update(online_weight, 'online_weight')
+      update(trended_weight, 'trended_weight')
+    })
+  }
+
+  await queue.onEmpty()
+
+  log(results)
+}
+
+module.exports = main
+
+if (!module.parent) {
+  const init = async () => {
+    try {
+      await main()
+    } catch (err) {
+      log(err)
+    }
+    process.exit()
+  }
+
+  try {
+    init()
+  } catch (err) {
+    log(err)
+    process.exit()
+  }
+}
--- server/README.md
+++ server/README.md
@@ -1,4 +1,5 @@
 # Log Rotation
+
 ```
 pm2 install pm2-logrotate
 ```
