cebe49d03a9d1879597218bb963f057f0a0331ad
mistakia <1823355+mistakia@users.noreply.github.com>
2022-05-24 10:57:21 -0400

    fix: morgan

--- server.js
+++ server.js
@@ -4,7 +4,14 @@ const logger = debug('server')
 const server = require('./api')
 const { port } = require('./config')
 
-debug.enable('server')
+const IS_DEV = process.env.NODE_ENV === 'development'
+const IS_PROD = process.env.NODE_ENV === 'production'
+
+if (IS_DEV) {
+  debug.enable('server,api*,knex:*')
+} else if (IS_PROD) {
+  debug.enable('server,api*')
+}
 
 const main = async () => {
   server.listen(port, () => logger(`API listening on port ${port}`))
