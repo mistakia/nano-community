7cd3ee7e5bf755903e52211bb27f9800e41248ca
mistakia <1823355+mistakia@users.noreply.github.com>
2024-02-12 22:23:08 -0500

    fix: limit import accounts meta script to 5 minutes and sort reps by weight

--- scripts/import-accounts-meta.mjs
+++ scripts/import-accounts-meta.mjs
@@ -12,13 +12,28 @@ const timestamp = Math.round(Date.now() / 1000)
 const importAccountsMeta = async () => {
   const { representatives } = await rpc.representatives()
   const rows = await db('accounts')
-    .select('account')
-    .where({ representative: true })
-    .where('last_seen', '>', dayjs().subtract('1', 'month').unix())
+    .join(
+      'accounts_meta_index',
+      'accounts.account',
+      '=',
+      'accounts_meta_index.account'
+    )
+    .select('accounts.account')
+    .where('accounts.representative', true)
+    .andWhere('accounts.last_seen', '>', dayjs().subtract(1, 'month').unix())
+    .orderBy('accounts_meta_index.weight', 'desc')
   const accounts = rows.map((r) => r.account)
 
   const inserts = []
+  const startTime = process.hrtime()
   for (const account of accounts) {
+    const currentTime = process.hrtime(startTime)
+    // Check if 5 minutes have passed (5 minutes = 300 seconds, hrtime returns time in seconds and nanoseconds)
+    if (currentTime[0] > 300) {
+      logger('5 minute runtime limit reached, stopping further processing')
+      break
+    }
+
     const accountInfo = await rpc.accountInfo({ account, timeout: 5000 })
     if (!accountInfo || accountInfo.error) {
       logger(`unable to get account info for ${account}`)
