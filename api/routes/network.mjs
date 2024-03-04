673dda6cf41e6f88404860dc59ab8b9938476c3f
mistakia <1823355+mistakia@users.noreply.github.com>
2024-03-04 09:59:42 -0500

    refactor: cache `api/network` endpoint

--- api/routes/network.mjs
+++ api/routes/network.mjs
@@ -1,8 +1,40 @@
 import express from 'express'
+import cron from 'node-cron'
 
 import { request } from '#common'
+import cache from '#api/cache.mjs'
 
 const router = express.Router()
+
+const load_network = async () => {
+  const requests = [
+    request({ url: 'https://stats.nanobrowse.com/json/stats.json' }), // TODO remove dependency
+    request({ url: 'https://nanolooker.com/api/market-statistics' }),
+    request({ url: 'https://stats.nanobrowse.com/json/monitors.json' }) // TODO remove dependency
+  ]
+
+  const responses = await Promise.allSettled(requests)
+  const fulfilled = responses.filter((r) => r.status === 'fulfilled')
+  if (!fulfilled.length) {
+    throw new Error('requests failed')
+  }
+
+  const stats = fulfilled.reduce((obj, v) => {
+    if (Array.isArray(v.value)) {
+      return { peers: v.value, ...obj }
+    }
+
+    // exclude priceStats
+    const { priceStats, ...rest } = v.value
+    return { ...rest, ...obj }
+  }, {})
+  cache.set('stats', stats, 300)
+  return stats
+}
+
+cron.schedule('*/5 * * * *', async () => {
+  await load_network()
+})
 
 router.get('/', async (req, res) => {
   const { logger, cache } = req.app.locals
@@ -12,28 +44,8 @@ router.get('/', async (req, res) => {
       return res.status(200).send(cachedStats)
     }
 
-    const requests = [
-      request({ url: 'https://stats.nanobrowse.com/json/stats.json' }), // TODO remove dependency
-      request({ url: 'https://nanolooker.com/api/market-statistics' }),
-      request({ url: 'https://stats.nanobrowse.com/json/monitors.json' }) // TODO remove dependency
-    ]
+    const stats = await load_network()
 
-    const responses = await Promise.allSettled(requests)
-    const fulfilled = responses.filter((r) => r.status === 'fulfilled')
-    if (!fulfilled.length) {
-      throw new Error('requests failed')
-    }
-
-    const stats = fulfilled.reduce((obj, v) => {
-      if (Array.isArray(v.value)) {
-        return { peers: v.value, ...obj }
-      }
-
-      // exclude priceStats
-      const { priceStats, ...rest } = v.value
-      return { ...rest, ...obj }
-    }, {})
-    cache.set('stats', stats, 60)
     res.status(200).send(stats)
   } catch (error) {
     logger(error)
