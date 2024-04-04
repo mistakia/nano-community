d64d724a4acd939098cf2d2f6c6774840691065e
mistakia <1823355+mistakia@users.noreply.github.com>
2024-04-03 22:14:59 -0400

    fix: locale race condition on doc page load

--- src/views/components/change-locale/change-locale.js
+++ src/views/components/change-locale/change-locale.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import FormControl from '@material-ui/core/FormControl'
-import Select from '@material-ui/core/Select'
-import MenuItem from '@material-ui/core/MenuItem'
-import SvgIcon from '@material-ui/core/SvgIcon'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import MenuItem from '@mui/material/MenuItem'
+import SvgIcon from '@mui/material/SvgIcon'
 
 import './change-locale.styl'
 
