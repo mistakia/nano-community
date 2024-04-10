4c9968ccdb852690238a68dec470fd77320af87f
mistakia <1823355+mistakia@users.noreply.github.com>
2024-04-10 00:52:57 -0400

    chore: upgrade node from `v14.17.0` to `v17.9.1`

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -20,7 +20,7 @@ module.exports = {
       'pre-deploy': 'git pull',
       'pre-deploy-local': '',
       'post-deploy':
-        'source /root/.bash_profile && /root/.nvm/versions/node/v14.17.0/bin/yarn install && pm2 reload ecosystem.config.js --env production',
+        'source /root/.bash_profile && /root/.nvm/versions/node/v17.9.1/bin/yarn install && pm2 reload ecosystem.config.js --env production',
       'pre-setup': ''
     }
   }
