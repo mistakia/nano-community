5da5a62bc37f6dea3b8ff1241dc2d2b216f446b6
mistakia <1823355+mistakia@users.noreply.github.com>
2024-05-30 21:06:31 -0400

    fix: remove google analytics

--- src/core/app/sagas.js
+++ src/core/app/sagas.js
@@ -1,29 +1,13 @@
-/* global gtag */
 import { takeLatest, fork, select } from 'redux-saga/effects'
 import { LOCATION_CHANGE } from 'connected-react-router'
-import FingerprintJS from '@fingerprintjs/fingerprintjs'
 
 import history from '@core/history'
 import { getApp } from './selectors'
 import { appActions } from './actions'
 
-const fpPromise = FingerprintJS.load()
-
-// cookie-less / anonymous GA reporting
 async function pageView() {
-  if (!window.gtag) {
-    return
-  }
-
-  const fp = await fpPromise
-  const result = await fp.get()
-
-  gtag('config', 'G-CV3N29BMWR', {
-    page_path: history.location.pathname,
-    client_storage: 'none',
-    anonymize_ip: true,
-    client_id: result.visitorId
-  })
+  console.log(`pageView: ${history.location.pathname}`)
+  // TODO
 }
 
 export function* init() {
